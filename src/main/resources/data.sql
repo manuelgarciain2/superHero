464d074d48b9b5d1e3baf44a4a728e7a8b6d0a51
Manuel IN2 <Ma0107G!>
2021-11-29 23:13:08 +0100

    configuration os database and initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into superheroes (first_name, last_name, super_hero_name)values('Peter','Parker','Spiderman');
+insert into superheroes (first_name, last_name, super_hero_name)values('Bruce','Banner','Hulk');
+insert into superheroes (first_name, last_name, super_hero_name)values('Bruce','Waine','Batman');
+insert into superheroes (first_name, last_name, super_hero_name)values('Tonny','Stark','IronMan');
+insert into superheroes (first_name, last_name, super_hero_name)values('Natasha','Romanoff','Black Widow');
+insert into superheroes (first_name, last_name, super_hero_name)values('Clark','Ken','SuperMan');
+insert into superheroes (first_name, last_name, super_hero_name)values('Diana','Princess','Wonder Woman');
+insert into superheroes (first_name, last_name, super_hero_name)values('Steve','Roger','Captain America');
