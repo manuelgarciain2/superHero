5393624fc9b2e30be8c8772d97e1638485b467d9
Manuel IN2 <Ma0107G!>
2021-11-29 21:59:22 +0100

    first commit

---
+++ src/main/java/com/in2/superHeroe/SuperHeroe/SuperHeroeApplication.java
@@ -0,0 +1,13 @@
+package com.in2.superHeroe.SuperHeroe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in2/superHeroe/SuperHeroe/SuperHeroeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in2.superHeroe.SuperHeroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
