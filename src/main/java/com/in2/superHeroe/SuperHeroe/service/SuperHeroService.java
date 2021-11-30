8f9a602ec03af77940c076225b037c1679dccb7d
Manuel IN2 <Ma0107G!>
2021-11-30 21:03:51 +0100

    Creating super hero services

---
+++ src/main/java/com/in2/superHeroe/SuperHeroe/service/SuperHeroService.java
@@ -0,0 +1,44 @@
+package com.in2.superHeroe.SuperHeroe.service;
+
+import com.in2.superHeroe.SuperHeroe.model.SuperHero;
+import com.in2.superHeroe.SuperHeroe.repository.SuperHerorepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SuperHeroService {
+    private SuperHero superHero;
+    @Autowired
+    private SuperHerorepository superHerorepository;
+
+    public List<SuperHero> findAllSuperHeroes() {
+        return superHerorepository.findAll();
+    }
+
+    public SuperHero findSuperheroId(Long id) {
+        return superHerorepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Superhero", "ID", id));
+    }
+
+    public SuperHero createSuperHero(SuperHero superHeroRequest) {
+        return superHerorepository.save(superHeroRequest);
+    }
+
+    public SuperHero updateSuperHero(SuperHero superHeroRequest) {
+        return superHerorepository.save(superHeroRequest);
+    }
+
+    public ApiResponse deleteSuperHero(SuperHero superHeroRequest) {
+        return superHerorepository.findById(superHeroRequest.getId()).map(superhero -> {
+            superHerorepository.delete(superHeroRequest);
+            return new ApiResponse(true, "Superhero deleted");
+        }).orElseThrow(() -> new ResourceNotFoundException("Superhero", "id", superHeroRequest.getId()));
+
+    }
+
+    public List<SuperHero> findAllContaingName(String name) {
+        return superHerorepository.findBysuperHeroNameContaining(name);
+    }
+
+}
